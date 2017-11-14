5deed2335274b3a4b1966d3232621bc779772f40
harcioger <harciog_hams@mail.ru>
2017-11-14 20:58:28 +0300

    FAQ page

---
+++ src/pages/faq/FaqItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <main-layout>
+    <div class="faq__question" @click="toggleItem">
+      <span class="faq__question__text">{{faqItem.problem}}</span>            
+    </div>
+    <div class="faq__answer" v-show="toggled">{{faqItem.answer}}</div>
+  </main-layout>
+</template>
+
+<script>
+  export default {
+    props:{
+      faqItem: {
+        type: Object,
+        default: {}
+      }
+    },
+    data() {
+      return {
+        toggled: false
+      }
+    },
+    methods: {
+      toggleItem: function() {
+        this.toggled = !this.toggled;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.faq__question{
+  background: #eee;
+  border-radius: 3px;
+  padding: 10px;
+  box-shadow: 1px 1px 3px rgba(0,0,0,0.3);
+}
+.faq__question__text{
+  border-bottom: 1px dotted #801f25;
+}
+.faq__answer{
+  padding: 10px;
+}
+</style>
