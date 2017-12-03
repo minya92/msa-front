69ab8838c8a5d863c6b00bb0b62bd59168fab640
Alexey Kashintsev <akashintsev@yandex.ru>
2017-12-03 15:56:54 +0300

    changed doAuth function

--- src/components/auth/do-auth.js
+++ src/components/auth/do-auth.js
@@ -1,39 +1,23 @@
 export const doAuth = (login, pass) => {
   return new Promise(function(resolve, reject) {
+      var newUrl = '/auth/';
       var xhr = new XMLHttpRequest();
-      xhr.open("get", '/auth', true);
+      xhr.open("get", newUrl, true);
+      xhr.setRequestHeader("Authorization", "Basic " + btoa(login+':'+pass));
       xhr.withCredentials = true;
       xhr.onreadystatechange = function(evt) {
           if (xhr.readyState == 4) {
-              resolve();
+              if (xhr.responseText == 'ok')
+                  resolve();
+              else
+                  reject(xhr);
           }
       };
       xhr.send();
-  }).then(function(urlParams) {
-      return new Promise(function(resolve, reject) {
-          var xhr = new XMLHttpRequest();
-          var stage = 0;
-
-          var body = 'j_username=' + encodeURIComponent(login) +
-            '&j_password=' + encodeURIComponent(pass);
-
-          xhr.open("POST", 'api/j_security_check', true);
-          xhr.withCredentials = true;
-          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-          xhr.onreadystatechange = function(evt) {
-              if (xhr.readyState == 4) {
-                    if (xhr.responseText.indexOf('"j_password" id="password"') == -1) {
-                        resolve();
-                    } else {
-                        xhr.abort();
-                        reject();
-                    }
-              }
-          };
-
-          xhr.send(body);
-      });
+  }).then(function() {
+    window.location = '#';
+  }, function(xhr) {
+    console.log(xhr);
   });
 }
 
